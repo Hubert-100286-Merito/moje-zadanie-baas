fe7bbc337dc953ab232ecdd90ce786f3caeaba35
Hubert <wrx93068@student.wroclaw.merito.pl>
2025-11-15 17:35:39 +0100

    Remove defineApi usage; REST via API Gateway in backend.ts

--- amplify/functions/helloWorld.ts
+++ amplify/functions/helloWorld.ts
@@ -2,5 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const helloWorld = defineFunction({
   name: "helloWorld",
-  entry: "./src/helloWorld.ts", // ścieżka względem ROOT repo (obok amplify/)
+  entry: "./src/helloWorld.ts",
 });
