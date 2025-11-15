24de45d75582a92118bb198045561f85bb72950a
Hubert Przybylski - wrx93068 - Merito <168918394+Hubert-100286-Merito@users.noreply.github.com>
2025-11-15 17:46:28 +0100

    Update helloWorld.ts

--- amplify/functions/helloWorld.ts
+++ amplify/functions/helloWorld.ts
@@ -2,5 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const helloWorld = defineFunction({
   name: "helloWorld",
-  entry: "./src/helloWorld.ts",
+  entry: "../../src/helloWorld.ts", // <-- UWAGA: dwa '…/..' do katalogu głównego
 });
